493f903b27e1e4997c0c70cdcbd2015d68014862
maccyber <jonas.enge@gmail.com>
2017-01-30 22:40:40 +0100

    Refactor

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const { json, send } = require('micro')
 const getQuestions = require('./lib/get-questions')
 
 module.exports = async (req, res) => {
-  try {
-    const data = req.method === 'POST' ? await json(req) : parse(req.url, true).query
-    const opts = {
-      page: parseInt(data.page) || 1,
-      langCode: data.lang || 'en',
-      limit: parseInt(data.limit) || 5
-    }
-    send(res, 200, getQuestions(opts))
-  } catch (err) {
-    send(res, 500, err)
+  const data = req.method === 'POST' ? await json(req) : parse(req.url, true).query
+  const opts = {
+    url: req.encrypted ? `https://${req.headers.host}` : `http://${req.headers.host}`,
+    page: parseInt(data.page) || 1,
+    langCode: data.lang || 'en',
+    limit: parseInt(data.limit) || 5
   }
+  const q = getQuestions(opts)
+  let status = q.error ? 500 : 200
+  send(res, status, q)
 }
