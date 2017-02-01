1b5d15f782f5885165e332f978cf4a63f20a5e3e
Jonas Enge <jonas.enge@t-fk.no>
2017-02-01 17:47:40 +0100

    Quickfix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const getQuestions = require('./lib/get-questions')
 module.exports = async (req, res) => {
   const data = req.method === 'POST' ? await json(req) : parse(req.url, true).query
   const opts = {
-    url: req.encrypted ? `https://${req.headers.host}` : `http://${req.headers.host}`,
+    url: req.encrypted ? `https://${req.headers.host}` : `https://${req.headers.host}`,
     page: parseInt(data.page) || 1,
     langCode: data.lang || 'en',
     limit: parseInt(data.limit) || 5
