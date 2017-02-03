2990b094ba2360c0414ad83317aa9006c1c7ff5b
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-02-03 21:15:28 +0100

    Adds choices for both directions

---
+++ data/en/choices.js
@@ -0,0 +1,48 @@
+'use strict'
+
+module.exports = {
+  plus: [
+    {
+      text: 'Very Inacurate',
+      value: 1
+    },
+    {
+      text: 'Moderately Inacurate',
+      value: 2
+    },
+    {
+      text: 'Neither Accurate Nor Inaccurate',
+      value: 3
+    },
+    {
+      text: 'Moderately Accurate',
+      value: 4
+    },
+    {
+      text: 'Very Accurate',
+      value: 5
+    }
+  ],
+  minus: [
+    {
+      text: 'Very Inacurate',
+      value: 5
+    },
+    {
+      text: 'Moderately Inacurate',
+      value: 4
+    },
+    {
+      text: 'Neither Accurate Nor Inaccurate',
+      value: 3
+    },
+    {
+      text: 'Moderately Accurate',
+      value: 2
+    },
+    {
+      text: 'Very Accurate',
+      value: 1
+    }
+  ]
+}
---
+++ data/no/choices.js
@@ -0,0 +1,48 @@
+'use strict'
+
+module.exports = {
+  plus: [
+    {
+      text: 'Svært lite treffende',
+      value: 1
+    },
+    {
+      text: 'Lite treffende',
+      value: 2
+    },
+    {
+      text: 'Treffer verken bra eller dårlig',
+      value: 3
+    },
+    {
+      text: 'Ganske treffende',
+      value: 4
+    },
+    {
+      text: 'Veldig treffende',
+      value: 5
+    }
+  ],
+  minus: [
+    {
+      text: 'Svært lite treffende',
+      value: 5
+    },
+    {
+      text: 'Lite treffende',
+      value: 4
+    },
+    {
+      text: 'Treffer verken bra eller dårlig',
+      value: 3
+    },
+    {
+      text: 'Ganske treffende',
+      value: 2
+    },
+    {
+      text: 'Veldig treffende',
+      value: 1
+    }
+  ]
+}
