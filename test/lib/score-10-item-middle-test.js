d7290e45d6d45373ce4bf6e07851c921b2ee9841
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-02-05 09:45:43 +0100

    New tests. Removes old questions

--- test/lib/score-10-item-middle-test.js
+++ test/lib/score-10-item-middle-test.js
@@ -2,9 +2,11 @@
 
 const tap = require('tap')
 const calculateScore = require('../../lib/calculate-score')
-const template = require('../../data/en/questions-10-item-scale')
-const answers = require('../data/answers-10-item-median.json')
-const score = calculateScore({template: template, answers: answers})
+const compileQuestions = require('../../lib/compile-questions')
+const questions = compileQuestions({language: 'en', items: '10'})
+const answers = Array.from({length: 50}, (v, i) => i).map(num => Object.assign({id: num + 1, score: 3}))
+
+const score = calculateScore({template: questions, answers: answers})
 
 tap.equal(score['E'], 30, 'It calculates 30 for E')
 
