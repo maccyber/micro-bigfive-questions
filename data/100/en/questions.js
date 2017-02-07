aa88dc12a6622997a48b7a8a7bed7211bd65f5d2
maccyber <jonas.enge@gmail.com>
2017-02-07 18:51:33 +0100

    Changed facet to domain

--- data/100/en/questions.js
+++ data/100/en/questions.js
@@ -10,501 +10,501 @@ module.exports = [
   {
     text: 'Believe in the importance of art',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Have a vivid imagination',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Tend to vote for liberal political candidates',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Carry the conversation to a higher level',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Enjoy hearing new ideas',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Enjoy thinking about things',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Can say things beautifully',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Enjoy wild flights of fantasy',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Get excited by new ideas',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Have a rich vocabulary',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Am not interested in abstract ideas',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Do not like art',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Avoid philosophical discussions',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Do not enjoy going to art museums',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Tend to vote for conservative political candidates',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Do not like poetry',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Rarely look for a deeper meaning in things',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Believe that too much tax money goes to support artists',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Am not interested in theoretical discussions',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Have difficulty understanding abstract ideas',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Often feel blue',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Dislike myself',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Am often down in the dumps',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Have frequent mood swings',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Panic easily',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Am filled with doubts about things',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Feel threatened easily',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Get stressed out easily',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Fear for the worst',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Worry about things',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Seldom feel blue',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Feel comfortable with myself',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Rarely get irritated',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Am not easily bothered by things',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Am very pleased with myself',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Am relaxed most of the time',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Seldom get mad',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Am not easily frustrated',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Remain calm under pressure',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Rarely lose my composure',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Feel comfortable around people',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Make friends easily',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Am skilled in handling social situations',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Am the life of the party',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Know how to captivate people',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Start conversations',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Warm up quickly to others',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Talk to a lot of different people at parties',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: "Don't mind being the center of attention",
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Cheer people up',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Have little to say',
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Keep in the background',
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Would describe my experiences as somewhat dull',
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: "Don't like to draw attention to myself",
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: "Don't talk a lot",
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Avoid contacts with others',
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Am hard to get to know',
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Retreat from others',
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Find it difficult to approach others',
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Keep others at a distance',
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Am always prepared',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Pay attention to details',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Get chores done right away',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Carry out my plans',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Make plans and stick to them',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Complete tasks successfully',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Do things according to a plan',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Am exacting in my work',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Finish what I start',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Follow through with my plans',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Waste my time',
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Find it difficult to get down to work',
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Do just enough work to get by',
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: "Don't see things through",
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Shirk my duties',
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Mess things up',
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Leave things unfinished',
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: "Don't put my mind on the task at hand",
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Make a mess of things',
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Need a push to get started',
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Have a good word for everyone',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Believe that others have good intentions',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Respect others',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Accept people as they are',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Make people feel at ease',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Am concerned about others',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Trust what people say',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: "Sympathize with others' feelings",
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Am easy to satisfy',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Treat all people equally',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Have a sharp tongue',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Cut others to pieces',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Suspect hidden motives in others',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Get back at others',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Insult people',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Believe that I am better than others',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Contradict others',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Make demands on others',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Hold a grudge',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Am out for my own personal gain',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   }
 ]
--- data/50/en/questions.js
+++ data/50/en/questions.js
@@ -10,251 +10,251 @@ module.exports = [
   {
     text: 'Believe in the importance of art',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Have a vivid imagination',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Tend to vote for liberal political candidates',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Carry the conversation to a higher level',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Enjoy hearing new ideas',
     choises: plus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Am not interested in abstract ideas',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Do not like art',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Avoid philosophical discussions',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Do not enjoy going to art museums',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Tend to vote for conservative political candidates',
     choises: minus,
-    facet: 'O'
+    domain: 'O'
   },
   {
     text: 'Often feel blue',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Dislike myself',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Am often down in the dumps',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Have frequent mood swings',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Panic easily',
     choises: plus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Rarely get irritated',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Seldom feel blue',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Feel comfortable with myself',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Am not easily bothered by things',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Am very pleased with myself',
     choises: minus,
-    facet: 'N'
+    domain: 'N'
   },
   {
     text: 'Feel comfortable around people',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Make friends easily',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Am skilled in handling social situations',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Am the life of the party',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Know how to captivate people',
     choises: plus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Have little to say',
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Keep in the background',
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Would describe my experiences as somewhat dull',
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: "Don't like to draw attention to myself",
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: "Don't talk a lot",
     choises: minus,
-    facet: 'E'
+    domain: 'E'
   },
   {
     text: 'Am always prepared',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Pay attention to details',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Get chores done right away',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Carry out my plans',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Make plans and stick to them',
     choises: plus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Waste my time',
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Find it difficult to get down to work',
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Do just enough work to get by',
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: "Don't see things through",
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Shirk my duties',
     choises: minus,
-    facet: 'C'
+    domain: 'C'
   },
   {
     text: 'Have a good word for everyone',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Believe that others have good intentions',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Respect others',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Accept people as they are',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Make people feel at ease',
     choises: plus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Have a sharp tongue',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Cut others to pieces"',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Suspect hidden motives in others',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Get back at others',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   },
   {
     text: 'Insult people',
     choises: minus,
-    facet: 'A'
+    domain: 'A'
   }
 ]
