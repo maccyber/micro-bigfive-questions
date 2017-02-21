8be6fc9840c0fd93a5b49db323605c752ebd6dbc
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-02-21 21:22:30 +0100

    Implements ava as testrunner

---
+++ test/lib/frontpage-test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import 'async-to-gen/register'
+const test = require('ava')
+const listen = require('test-listen')
+const axios = require('axios')
+const micro = require('micro')
+const srv = require('../../index')
+
+const getUrl = fn => {
+  const srv = micro(fn)
+  return listen(srv)
+}
+
+test('it returns README as frontpage', async t => {
+  const url = await getUrl(srv)
+  const result = await axios.get(url)
+  t.true(result.data.includes('MIT'), 'frontpage ok')
+})
