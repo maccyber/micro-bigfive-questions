6d47bebee460c17d60a094db01a32a35537cf1ba
maccyber <jonas.enge@gmail.com>
2017-03-04 01:44:34 +0100

    Adds swedish and code

---
+++ data/20/se/choices.js
@@ -0,0 +1,58 @@
+'use strict'
+
+module.exports = {
+  plus: [
+    {
+      text: 'Mycket felaktigt',
+      score: 1,
+      color: 1
+    },
+    {
+      text: 'Måttligt felaktigt',
+      score: 2,
+      color: 2
+    },
+    {
+      text: 'Varken korrekt eller felaktig',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Måttligt korrekt',
+      score: 4,
+      color: 4
+    },
+    {
+      text: 'Väldigt korrekt',
+      score: 5,
+      color: 5
+    }
+  ],
+  minus: [
+    {
+      text: 'Mycket felaktigt',
+      score: 5,
+      color: 1
+    },
+    {
+      text: 'Måttligt felaktigt',
+      score: 4,
+      color: 2
+    },
+    {
+      text: 'Varken korrekt eller felaktig',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Måttligt korrekt',
+      score: 2,
+      color: 4
+    },
+    {
+      text: 'Väldigt korrekt',
+      score: 1,
+      color: 5
+    }
+  ]
+}
--- data/20/se/questions.js
+++ data/20/se/questions.js
@@ -9,121 +9,121 @@ module.exports = [
     info: 'test info ...'
   },
   {
-    text: 'Am the life of the party',
+    text: 'Är den som håller igång på ett party',
     choises: plus,
     code: ['H34', 'X94'],
     domain: 'E'
   },
   {
-    text: 'Talk to a lot of different people at parties',
+    text: 'Pratar med en massa olika människor på fester',
     choises: plus,
     code: ['X83'],
     domain: 'E'
   },
   {
-    text: "Don't talk a lot",
+    text: 'Pratar inte mycket',
     choises: minus,
     code: ['X56'],
     domain: 'E'
   },
   {
-    text: 'Keep in the background',
+    text: 'Håller mig i bakgrunden',
     choises: minus,
     code: ['H154', 'X34'],
     domain: 'E'
   },
   {
-    text: "Sympathize with others' feelings.",
+    text: 'Sympatiserar med andras känslor',
     choises: plus,
     code: ['H1130'],
     domain: 'A'
   },
   {
-    text: "Feel others' emotions",
+    text: 'Känner andra människors känslor',
     choises: plus,
     code: ['E136'],
     domain: 'A'
   },
   {
-    text: 'Am not really interested in others',
+    text: 'Är egentligen inte intresserad av andra',
     choises: minus,
     code: ['X165'],
     domain: 'A'
   },
   {
-    text: "Am not interested in other people's problems",
+    text: 'Är inte intresserad av andra människors problem',
     choises: minus,
     code: ['X227'],
     domain: 'A'
   },
   {
-    text: 'Get chores done right away',
+    text: 'Fullföljer mina plikt så snart som möjligt',
     choises: plus,
     code: ['T321'],
     domain: 'C'
   },
   {
-    text: 'Like order',
+    text: 'Tycker om ordning',
     choises: plus,
     code: ['X118'],
     domain: 'C'
   },
   {
-    text: 'Often forget to put things back in their proper place',
+    text: 'Glömmer ofta att lägga tillbaka saker på sin plats',
     choises: minus,
     code: ['X82'],
     domain: 'C'
   },
   {
-    text: 'Make a mess of things',
+    text: 'Skapar oreda omkring mig',
     choises: minus,
     code: ['H1467'],
     domain: 'C'
   },
   {
-    text: 'Have frequent mood swings',
+    text: 'Har ofta förekommande humörsvängningar',
     choises: plus,
     code: ['E92'],
     domain: 'N'
   },
   {
-    text: 'Get upset easily',
+    text: 'Blir lätt upprörd',
     choises: plus,
     code: ['X95'],
     domain: 'N'
   },
   {
-    text: 'Am relaxed most of the time',
+    text: 'Är avslappnad för det mesta',
     choises: minus,
     code: ['E141'],
     domain: 'N'
   },
   {
-    text: 'Seldom feel blue',
+    text: 'Känner mig sällan nere',
     choises: minus,
     code: ['X156'],
     domain: 'N'
   },
   {
-    text: 'Have a vivid imagination',
+    text: 'Har en livlig fantasi',
     choises: plus,
     code: ['X14'],
     domain: 'O'
   },
   {
-    text: 'Have difficulty understanding abstract ideas',
+    text: 'Har svårt att förstå abstrakta idéer',
     choises: minus,
     code: ['X176'],
     domain: 'O'
   },
   {
-    text: 'Am not interested in abstract ideas',
+    text: 'Är inte intresserad av abstrakta idéer',
     choises: minus,
     code: ['X228'],
     domain: 'O'
   },
   {
-    text: 'Do not have a good imagination',
+    text: 'Är fantasilös',
     choises: minus,
     code: ['X272'],
     domain: 'O'
